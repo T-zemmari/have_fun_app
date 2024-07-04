57f7a9c757beb03844ff13ddae064d86ce994726
Tarek <tarek@tedybusiness.com>
2024-07-04 15:53:36 +0200

    mejorando la jugabilidad de las damas

--- resources/views/juegos/damas.blade.php
+++ resources/views/juegos/damas.blade.php
@@ -82,7 +82,7 @@
         const computerColor = 'black';
 
         let selectedChecker = null;
-        let playerMoved = false; // Indicador para asegurar que el ordenador no mueva hasta que el jugador haya movido
+        let playerMoved = false;
 
         function generateBoard() {
             checkersBoard.innerHTML = '';
@@ -120,31 +120,22 @@
                 const startRow = parseInt(selectedChecker.parentElement.dataset.row);
                 const startCol = parseInt(selectedChecker.parentElement.dataset.col);
                 if (isValidMove(startRow, startCol, row, col, selectedChecker)) {
+                    const captured = capturePieceIfNeeded(startRow, startCol, row, col);
                     moveChecker(selectedChecker, cell);
+                    if (captured) {
+                        checkWin(playerColor);
+                    }
                     selectedChecker.classList.remove('selected');
                     selectedChecker = null;
-                    playerMoved = true; // El jugador ha movido
-                    if (checkWin(playerColor)) {
-                        Swal.fire({
-                            html: `<h4>¡Felicidades! Has ganado el juego de damas.</h4>`,
-                            icon: "success"
-                        });
-                        generateBoard();
-                    } else {
-                        setTimeout(() => {
-                            if (playerMoved) {
-                                computerMove();
-                                if (checkWin(computerColor)) {
-                                    Swal.fire({
-                                        html: `<h4>¡Lo siento! Has perdido el juego de damas.</h4>`,
-                                        icon: "error"
-                                    });
-                                    generateBoard();
-                                }
-                                playerMoved = false; // Resetear el indicador después del movimiento del ordenador
-                            }
-                        }, 500);
-                    }
+                    playerMoved = true;
+
+                    setTimeout(() => {
+                        if (playerMoved) {
+                            computerMove();
+                            checkWin(computerColor);
+                            playerMoved = false;
+                        }
+                    }, 500);
                 } else {
                     selectedChecker.classList.remove('selected');
                     selectedChecker = null;
@@ -160,8 +151,8 @@
             const distance = Math.abs(startRow - endRow);
 
             const targetCell = document.querySelector(`td[data-row="${endRow}"][data-col="${endCol}"]`);
-            if (targetCell && targetCell.firstChild && targetCell.firstChild.dataset.color === checker.dataset.color) {
-                return false; // No se puede mover a una celda con una pieza del mismo color
+            if (targetCell && targetCell.firstChild) {
+                return false;
             }
 
             if (distance === 1 && Math.abs(startCol - endCol) === 1) {
@@ -171,20 +162,29 @@
                 const middleCol = (startCol + endCol) / 2;
                 const middleCell = document.querySelector(`td[data-row="${middleRow}"][data-col="${middleCol}"]`);
                 if (middleCell && middleCell.firstChild && middleCell.firstChild.dataset.color !== checker.dataset.color) {
-                    return true; // Movimiento de captura válido
+                    return true;
                 }
             }
             return false;
         }
 
         function moveChecker(checker, destination) {
-            if (destination.childElementCount === 0) {
-                destination.appendChild(checker);
-                const row = parseInt(destination.dataset.row);
-                if ((row === 0 && checker.dataset.color === playerColor) || (row === boardSize - 1 && checker.dataset.color === computerColor)) {
-                    checker.classList.add('king');
-                }
+            destination.appendChild(checker);
+            const row = parseInt(destination.dataset.row);
+            if ((row === 0 && checker.dataset.color === playerColor) || (row === boardSize - 1 && checker.dataset.color === computerColor)) {
+                checker.classList.add('king');
             }
+        }
+
+        function capturePieceIfNeeded(startRow, startCol, endRow, endCol) {
+            const middleRow = (startRow + endRow) / 2;
+            const middleCol = (startCol + endCol) / 2;
+            const middleCell = document.querySelector(`td[data-row="${middleRow}"][data-col="${middleCol}"]`);
+            if (Math.abs(startRow - endRow) === 2 && middleCell && middleCell.firstChild) {
+                middleCell.removeChild(middleCell.firstChild);
+                return true;
+            }
+            return false;
         }
 
         function computerMove() {
@@ -204,17 +204,8 @@
                 const randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
                 const startCell = document.querySelector(`td[data-row="${randomMove.startRow}"][data-col="${randomMove.startCol}"]`);
                 const targetCell = document.querySelector(`td[data-row="${randomMove.move.row}"][data-col="${randomMove.move.col}"]`);
-                const capturedPiece = capturePieceIfNeeded(randomMove.startRow, randomMove.startCol, randomMove.move.row, randomMove.move.col);
+                capturePieceIfNeeded(randomMove.startRow, randomMove.startCol, randomMove.move.row, randomMove.move.col);
                 moveChecker(startCell.firstChild, targetCell);
-            }
-        }
-
-        function capturePieceIfNeeded(startRow, startCol, endRow, endCol) {
-            const middleRow = (startRow + endRow) / 2;
-            const middleCol = (startCol + endCol) / 2;
-            const middleCell = document.querySelector(`td[data-row="${middleRow}"][data-col="${middleCol}"]`);
-            if (Math.abs(startRow - endRow) === 2 && middleCell && middleCell.firstChild) {
-                middleCell.removeChild(middleCell.firstChild);
             }
         }
 
@@ -222,7 +213,6 @@
             const direction = checker.dataset.color === playerColor ? -1 : 1;
             const possibleMoves = [];
 
-            // Simple moves
             for (let dCol of [-1, 1]) {
                 const newRow = row + direction;
                 const newCol = col + dCol;
@@ -232,7 +222,6 @@
                 }
             }
 
-            // Capturing moves
             for (let dCol of [-2, 2]) {
                 const newRow = row + direction * 2;
                 const newCol = col + dCol;
@@ -254,8 +243,18 @@
             const computerCheckers = allCells.filter(cell => cell.firstChild && cell.firstChild.dataset.color === computerColor);
 
             if (color === playerColor && computerCheckers.length === 0) {
+                Swal.fire({
+                    html: `<h4>¡Felicidades! Has ganado el juego de damas.</h4>`,
+                    icon: "success"
+                });
+                generateBoard();
                 return true;
             } else if (color === computerColor && playerCheckers.length === 0) {
+                Swal.fire({
+                    html: `<h4>¡Lo siento! Has perdido el juego de damas.</h4>`,
+                    icon: "error"
+                });
+                generateBoard();
                 return true;
             }
             return false;
@@ -263,7 +262,7 @@
 
         resetGameBtn.addEventListener('click', () => {
             selectedChecker = null;
-            playerMoved = false; // Resetear el indicador al reiniciar el juego
+            playerMoved = false;
             generateBoard();
         });
 
