b4e405ffaefc80e51e42e2dca1fa8cb84509e05d
Tarek <tarek@tedybusiness.com>
2024-07-17 17:30:49 +0200

    se ha creado una nueva migracion y se ha subid para hacer pruebas en la base de datos

--- resources/views/admin/add-game.blade.php
+++ resources/views/admin/add-game.blade.php
@@ -1,3 +1,35 @@
 <x-admin-base>
 
+    <div class="w-full">
+
+
+        <form class="max-w-lg mx-auto mt-[100px] border border-gray-200 p-16">
+            <div class="mb-5">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Nombre del juego</label>
+                <input type="text" id="name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="Escribe el nombre del juego..." required />
+            </div>
+            <div class="mb-5">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Descripción</label>
+                <textarea type="text" id="description"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="Escribe una breve descripción..."></textarea>
+            </div>
+
+            <div class="mb-5">
+                <label class="inline-flex items-center mb-5 cursor-pointer">
+                    <input type="checkbox" value="" class="sr-only peer" checked>
+                    <div
+                        class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                    </div>
+                    <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Mostrar En Web</span>
+                </label>
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Guardar</button>
+        </form>
+
+    </div>
+
 </x-admin-base>
